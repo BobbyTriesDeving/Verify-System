e132e0912f0c7c0f01a074327c664ab4c962ad5f
Bobby Sanders <jaxxmwah@gmail.com>
2025-04-30 23:32:21 -0400

    Update sconfig.lua

--- sconfig.lua
+++ sconfig.lua
@@ -3,3 +3,12 @@ Config = {}
 Config.BotToken = ''
 Config.GuildId = ''
 Config.RoleId = ''
+
+Config.Webhook = ''
+Config.verifywebhook = ''
+
+
+--[[
+Config.Webhook = the embed that sends when you start the script for the first time
+Config.verifywebhook = the embed that sends when a player verifies
+]]
